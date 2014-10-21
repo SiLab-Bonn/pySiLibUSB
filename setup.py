316c0eceb5804c3463f19c061dda9cc8757e4df0
Jens Janssen <janssen@physik.uni-bonn.de>
2014-10-21 08:19:18 +0000

    ENH: use setuptools to make developer mode possible

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python   
 
-from distutils.core import setup
+from setuptools import setup
 
 from SiLibUSB import __version__
+
+author = 'Tomasz Hemperek'
+author_email = 'hemperek@uni-bonn.de'
 
 setup(
     name='pySiLibUSB',
@@ -11,6 +14,11 @@ setup(
     description='SILAB USB Device Application Programming Interface',
     url='https://silab-redmine.physik.uni-bonn.de/projects/pysilibusb',
     license = 'BSD 3-Clause ("BSD New" or "BSD Simplified") License',
-    long_description =''
+    long_description ='',
+    author=author,
+    maintainer=author,
+    author_email=author_email,
+    maintainer_email=author_email,
+    install_requires=['pyusb>=1.0.0b2'],
 )
 
