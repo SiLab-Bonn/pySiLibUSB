4b5f87e93db453360dc03719ca79254091590540
Jens Janssen <janssen@physik.uni-bonn.de>
2014-10-21 14:24:12 +0000

    ENH: more cleanup SVN: adding files for packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python   
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-from SiLibUSB import __version__
+from pysilibusb.siusbdevice import __version__
 
 author = 'Tomasz Hemperek'
 author_email = 'hemperek@uni-bonn.de'
@@ -10,15 +10,16 @@ author_email = 'hemperek@uni-bonn.de'
 setup(
     name='pySiLibUSB',
     version=__version__,
-    py_modules=['SiLibUSB'],
     description='SILAB USB Device Application Programming Interface',
     url='https://silab-redmine.physik.uni-bonn.de/projects/pysilibusb',
-    license = 'BSD 3-Clause ("BSD New" or "BSD Simplified") License',
-    long_description ='',
+    license='BSD 3-Clause ("BSD New" or "BSD Simplified") License',
+    long_description='',
+    install_requires=['pyusb>=1.0.0rc1'],
+    packages=find_packages(),
+    include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
+    package_data={'': ['*.txt', 'VERSION'], 'examples': ['*']},
     author=author,
     maintainer=author,
     author_email=author_email,
     maintainer_email=author_email,
-    install_requires=['pyusb>=1.0.0b2'],
 )
-
