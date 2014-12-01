41b752ce624f15cb9193e6338d5392f75ec0d7a3
Jens Janssen <janssen@physik.uni-bonn.de>
2014-12-01 20:28:38 +0000

    MAINT: cleanup

--- SiLibUSB/siusbdevice.py
+++ SiLibUSB/siusbdevice.py
@@ -57,9 +57,9 @@ __version__ = '2.0.0'
 __version_info__ = (tuple([int(num) for num in __version__.split('.')]), 'final', 0)
 
 # set debugging options for pyUSB
-#import os
-#os.environ['PYUSB_DEBUG_LEVEL'] = 'debug'
-#os.environ['PYUSB_DEBUG'] = 'debug'
+# import os
+# os.environ['PYUSB_DEBUG_LEVEL'] = 'debug'
+# os.environ['PYUSB_DEBUG'] = 'debug'
 
 import usb.core
 import usb.util
