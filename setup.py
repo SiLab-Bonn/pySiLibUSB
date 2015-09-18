17b12aa2e9c6c78b39abc34c5fb7165af005e58b
Jens Janssen <janssen@physik.uni-bonn.de>
2015-09-18 10:24:55 +0000

    MAINT: update setup description

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ author_email = 'hemperek@uni-bonn.de, janssen@physik.uni-bonn.de'
 setup(
     name='pySiLibUSB',
     version=__version__,
-    description='SILAB USB Device Application Programming Interface',
+    description='pySiLibUSB - SILAB USB device pplication programming interface',
     url='https://silab-redmine.physik.uni-bonn.de/projects/pysilibusb',
     license='BSD 3-Clause ("BSD New" or "BSD Simplified") License',
-    long_description='',
+    long_description='API for SILAB USB devices.',
     install_requires=['pyusb>=1.0.0rc1'],
     packages=find_packages(),
     include_package_data=True,  # accept all data files and directories matched by MANIFEST.in or found in source control
@@ -25,4 +25,5 @@ setup(
     maintainer=author,
     author_email=author_email,
     maintainer_email=author_email,
+    platforms='any'
 )
